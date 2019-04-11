3ff7b258f276fcf09371978feccfce904cee8565
JMLegere <jeremy.m.legere@gmail.com>
2019-04-10 21:01:52 -0300

    Create All Pages

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Contact from './views/Contact.vue';
+
 
 Vue.use(Router);
 
@@ -14,10 +17,22 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
+      component: About,
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact,
+    },
+    {
+      path: '/work',
+      name: 'work',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      // I did this because this page has more content than the others.
+      component: () => import(/* webpackChunkName: "about" */ './views/Work.vue'),
     },
+
   ],
 });
